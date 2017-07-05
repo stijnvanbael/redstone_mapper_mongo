36797568d88a04bb3ca4eef9d9a81f40ba300b78
s.vanbael <s.vanbael@ccvlab.eu>
2017-07-05 14:47:20 +0200

    Book activity manually [BAM]

--- lib/manager.dart
+++ lib/manager.dart
@@ -2,69 +2,69 @@ library mongodb_manager;
 
 import 'dart:async';
 
+import 'package:connection_pool/connection_pool.dart';
+import 'package:mongo_dart/mongo_dart.dart';
 import 'package:redstone_mapper/database.dart';
 import 'package:redstone_mapper/mapper.dart';
-import 'package:connection_pool/connection_pool.dart';
 import 'package:redstone_mapper_mongo/metadata.dart';
-import 'package:mongo_dart/mongo_dart.dart';
 
 ///Manage connections with a MongoDB instance
 class MongoDbManager implements DatabaseManager<MongoDb> {
-  
+
   _MongoDbPool _pool;
-  
+
   /**
    * Creates a new MongoDbManager
-   * 
+   *
    * [uri] a MongoDB uri, and [poolSize] is the number of connections
    * that will be created.
-   * 
-   */ 
+   *
+   */
   MongoDbManager(String uri, {int poolSize: 3}) {
     _pool = new _MongoDbPool(uri, poolSize);
   }
-  
+
   @override
   void closeConnection(MongoDb connection, {error}) {
     var invalidConn = error is ConnectionException;
     _pool.releaseConnection(
-        connection._managedConn, 
+        connection._managedConn,
         markAsInvalid: invalidConn);
   }
 
   @override
   Future<MongoDb> getConnection() {
     return _pool.getConnection().then((managedConn) =>
-      new MongoDb(managedConn));
+    new MongoDb(managedConn));
   }
 }
 
 /**
  * Wrapper for the MongoDb driver.
- * 
+ *
  * This class provides helper functions for
  * enconding query parameters and decoding query
  * results using redstone_mapper.
- * 
- */ 
+ *
+ */
 class MongoDb {
-  
+
   ManagedConnection _managedConn;
-  
+
   MongoDb(this._managedConn);
 
   ///The original MongoDb connection object.
   Db get innerConn => _managedConn.conn;
-  
+
   ///get a MongoDb collection
   DbCollection collection(String collectionName) {
     return innerConn.collection(collectionName);
   }
-  
+
   ///Encode [data] to a Map or List.
   dynamic encode(dynamic data) =>
       _codec.encode(data);
-  
+
   ///Decode [data] to one or more objects of type [type].
   dynamic decode(dynamic data, Type type) =>
       _codec.decode(data, type);
@@ -100,14 +100,25 @@ class MongoDb {
     return dbCol.find(selector).map((result) =>
         _codec.decode(result, type));
   }
-  
+
+  /**
+   * Wrapper for DbCollection.distinct().
+   */
+  Future<List<dynamic>> distinct(dynamic collection, String field, [dynamic selector]) {
+    var dbCol = _collection(collection);
+    if (selector != null && selector is! Map && selector is! SelectorBuilder) {
+      selector = _codec.encode(selector);
+    }
+    return dbCol.distinct(field, selector).then((result) => result['values']);
+  }
+
   /**
    * Wrapper for DbCollection.findOne().
-   * 
+   *
    * [collection] is the MongoDb collection where the query will be executed,
    * and it can be a String or a DbCollection. [selector] can be a Map, a SelectorBuilder,
    * or an encodable object. The query result will be decoded to an object of type [type]
-   */ 
+   */
   Future findOne(dynamic collection, Type type, [dynamic selector]) {
     var dbCol = _collection(collection);
     if (selector != null && selector is! Map && selector is! SelectorBuilder) {
@@ -131,14 +142,14 @@ class MongoDb {
     }
     return dbCol.count(selector);
   }
-  
+
   /**
    * Wrapper for DbCollection.save().
-   * 
+   *
    * [collection] is the MongoDb collection where the query will be executed,
    * and it can be a String or a DbCollection. [obj] is the object to be saved,
    * and can be a Map or an encodable object.
-   */ 
+   */
   Future save(dynamic collection, Object obj) {
     var dbCol = _collection(collection);
     if (obj is! Map) {
@@ -146,14 +157,14 @@ class MongoDb {
     }
     return dbCol.save(obj);
   }
-  
+
   /**
    * Wrapper for DbCollection.insert().
-   * 
+   *
    * [collection] is the MongoDb collection where the query will be executed,
    * and it can be a String or a DbCollection. [obj] is the object to be inserted,
    * and can be a Map or an encodable object.
-   */ 
+   */
   Future insert(dynamic collection, Object obj) {
     var dbCol = _collection(collection);
     if (obj is! Map) {
@@ -161,32 +172,32 @@ class MongoDb {
     }
     return dbCol.insert(obj);
   }
-  
+
   /**
    * Wrapper for DbCollection.insertAll().
-   * 
+   *
    * [collection] is the MongoDb collection where the query will be executed,
    * and it can be a String or a DbCollection. [objs] are the objects to be inserted,
    * and can be a list of maps, or a list of encodable objects.
-   */ 
+   */
   Future insertAll(dynamic collection, List objs) {
     var dbCol = _collection(collection);
     return dbCol.insertAll(_codec.encode(objs));
   }
-  
+
   /**
    * Wrapper for DbCollection.update().
-   * 
+   *
    * [collection] is the MongoDb collection where the query will be executed,
    * and it can be a String or a DbCollection. [selector] can be a Map, a SelectorBuilder,
-   * or an encodable object. [obj] is the object to be updated, and can be a Map, a 
-   * ModifierBuilder or an encodable object. If [obj] is an encodable object and 
+   * or an encodable object. [obj] is the object to be updated, and can be a Map, a
+   * ModifierBuilder or an encodable object. If [obj] is an encodable object and
    * [override] is false, then the codec will produce a ModifierBuilder, and only
    * non null fields will be updated, otherwise, the entire document will be updated.
-   */ 
-  Future update(dynamic collection, dynamic selector, Object obj, {bool override: true, 
-                                               bool upsert: false, 
-                                               bool multiUpdate: false}) {
+   */
+  Future update(dynamic collection, dynamic selector, Object obj, {bool override: true,
+    bool upsert: false,
+    bool multiUpdate: false}) {
     var dbCol = _collection(collection);
     if (selector != null && selector is! Map && selector is! SelectorBuilder) {
       selector = _codec.encode(selector);
@@ -198,17 +209,17 @@ class MongoDb {
         obj = _updtCodec.encode(obj);
       }
     }
-    return dbCol.update(selector, obj, 
-          upsert: upsert, multiUpdate: multiUpdate);
+    return dbCol.update(selector, obj,
+        upsert: upsert, multiUpdate: multiUpdate);
   }
-  
+
   /**
    * Wrapper for DbCollection.remove().
-   * 
+   *
    * [collection] is the MongoDb collection where the query will be executed,
    * and it can be a String or a DbCollection. [selector] can be a Map, a SelectorBuilder,
    * or an encodable object.
-   */ 
+   */
   Future remove(dynamic collection, dynamic selector) {
     var dbCol = _collection(collection);
     if (selector is! Map) {
@@ -216,7 +227,7 @@ class MongoDb {
     }
     return dbCol.remove(selector);
   }
-  
+
   DbCollection _collection(collection) {
     if (collection is String) {
       collection = innerConn.collection(collection);
@@ -226,11 +237,11 @@ class MongoDb {
 }
 
 class _MongoDbPool extends ConnectionPool<Db> {
-  
+
   String uri;
-  
+
   _MongoDbPool(String this.uri, int poolSize) : super(poolSize);
-  
+
   @override
   void closeConnection(Db conn) {
     conn.close();
@@ -244,8 +255,8 @@ class _MongoDbPool extends ConnectionPool<Db> {
 }
 
 
-FieldDecoder _fieldDecoder = (Object data, String fieldName, 
-                              Field fieldInfo, List metadata) {
+FieldDecoder _fieldDecoder = (Object data, String fieldName,
+    Field fieldInfo, List metadata) {
   String name = fieldInfo.model;
   if (name == null) {
     name = fieldName;
@@ -261,8 +272,8 @@ FieldDecoder _fieldDecoder = (Object data, String fieldName,
   return value;
 };
 
-FieldEncoder _fieldEncoder = (Map data, String fieldName, Field fieldInfo, 
-                              List metadata, Object value) {
+FieldEncoder _fieldEncoder = (Map data, String fieldName, Field fieldInfo,
+    List metadata, Object value) {
   String name = fieldInfo.model;
   if (name == null) {
     name = fieldName;
@@ -282,10 +293,10 @@ FieldEncoder _fieldEncoder = (Map data, String fieldName, Field fieldInfo,
   }
 };
 
-FieldEncoder _updtFieldEncoder = (Map data, String fieldName, Field fieldInfo, 
-                                  List metadata, Object value) {
+FieldEncoder _updtFieldEncoder = (Map data, String fieldName, Field fieldInfo,
+    List metadata, Object value) {
   if (value == null) {
-   return;
+    return;
   }
   String name = fieldInfo.model;
   if (name == null) {
@@ -313,7 +324,7 @@ FieldEncoder _updtFieldEncoder = (Map data, String fieldName, Field fieldInfo,
   }
 };
 
-GenericTypeCodec _codec = new GenericTypeCodec(fieldDecoder: _fieldDecoder, 
-                                               fieldEncoder: _fieldEncoder);
+GenericTypeCodec _codec = new GenericTypeCodec(fieldDecoder: _fieldDecoder,
+    fieldEncoder: _fieldEncoder);
 
 GenericTypeCodec _updtCodec = new GenericTypeCodec(fieldEncoder: _updtFieldEncoder);
--- lib/service.dart
+++ lib/service.dart
@@ -2,86 +2,97 @@ library mongodb_service;
 
 import 'dart:async';
 
+import 'package:mongo_dart/mongo_dart.dart';
 import 'package:redstone/redstone.dart';
 import 'package:redstone_mapper_mongo/manager.dart';
-import 'package:mongo_dart/mongo_dart.dart';
 
 /**
  * Handles MongoDB operations for type T.
- * 
+ *
  * Usage:
- * 
+ *
  *      MongoDbService dbService = new MongoDbService<User>("users");
- * 
+ *
  *      @app.Route("/services/user/list")
  *      @Encode()
  *      Future<List<User>> listUsers() => dbService.find();
- * 
+ *
  *      @app.Route("/services/user/add")
- *      Future addUser(@Decode() User user) => 
+ *      Future addUser(@Decode() User user) =>
  *          dbService.insert(user);
- * 
+ *
  * Also, it's possible to inherit from this class:
- * 
+ *
  *      @app.Group("/services/user")
  *      Class UserServices extends MongoDbService<User> {
- * 
+ *
  *        UserServices() : super("users");
- * 
+ *
  *        @app.Route("/list")
  *        @Encode()
  *        Future<List<User>> list() => find();
- *        
+ *
  *        @app.Route("/add")
  *        Future add(@Decode() User user) => insert(user);
- * 
+ *
  *      }
- * 
+ *
  * By default, the service will use the database connection
  * associated with the current http request. If you are not using
- * Redstone.dart, be sure to use the [MongoDbService.fromConnection] 
+ * Redstone.dart, be sure to use the [MongoDbService.fromConnection]
  * constructor to create a new service.
- * 
- */ 
+ *
+ */
 class MongoDbService<T> {
-  
+
   MongoDb _mongoDb = null;
-  
+
   ///The name of the MongoDB collection associated with this service
   final String collectionName;
-  
+
   ///The MongoDB connection wrapper
-  MongoDb get mongoDb => 
+  MongoDb get mongoDb =>
       _mongoDb != null ? _mongoDb : request.attributes["dbConn"];
-  
+
   ///The MongoDb connection
   Db get innerConn => mongoDb.innerConn;
-  
+
   ///The MongoDB collection associated with this service
   DbCollection get collection => mongoDb.collection(collectionName);
-  
+
   /**
    * Creates a new MongoDB service.
-   * 
+   *
    * This service will use the database connection
    * associated with the current http request.
-   */ 
+   */
   MongoDbService(this.collectionName);
-  
+
   /**
    * Creates a new MongoDB service, using the provided
    * MongoDB connection.
    */
   MongoDbService.fromConnection(this._mongoDb, this.collectionName);
-  
+
   /**
    * Wrapper for DbCollection.find().
-   * 
+   *
    * [selector] can be a Map, a SelectorBuilder,
    * or an encodable object.
    */
   Future<List<T>> find([dynamic selector]) {
     return mongoDb.find(collection, T, selector);
+  }
+
+
+  /**
+   * Wrapper for DbCollection.distinct()
+   *
+   * [selector] can be a Map, a SelectorBuilder,
+   * or an encodable object.
+   */
+  Future<List<dynamic>> distinct(String field, [dynamic selector]) {
+    return mongoDb.distinct(collection, field, selector);
   }
 
   /**
@@ -106,63 +117,63 @@ class MongoDbService<T> {
 
   /**
    * Wrapper for DbCollection.findOne().
-   * 
+   *
    * [selector] can be a Map, a SelectorBuilder,
    * or an encodable object.
-   */ 
+   */
   Future<T> findOne([dynamic selector]) {
     return mongoDb.findOne(collection, T, selector);
   }
-  
+
   /**
    * Wrapper for DbCollection.save().
-   * 
+   *
    * [obj] is the object to be saved.
-   */ 
+   */
   Future save(T obj) {
     return mongoDb.save(collection, obj);
   }
-  
+
   /**
    * Wrapper for DbCollection.insert().
-   * 
+   *
    * [obj] is the object to be inserted.
-   */ 
+   */
   Future insert(T obj) {
-    return mongoDb.insert(collection, obj); 
+    return mongoDb.insert(collection, obj);
   }
-  
+
   /**
    * Wrapper for DbCollection.insertAll().
-   * 
+   *
    * [objs] are the objectes to be inserted.
-   */ 
+   */
   Future insertAll(List<T> objs) {
     return mongoDb.insertAll(collection, objs);
   }
-  
+
   /**
    * Wrapper for DbCollection.update().
-   * 
+   *
    * [selector] can be a Map, a SelectorBuilder,
-   * or an encodable object. [obj] is the object to be updated, and 
+   * or an encodable object. [obj] is the object to be updated, and
    * if [override] is false, then only non null fields will be updated,
    * otherwise, the entire document will be replaced.
    */
-  Future update(dynamic selector, T obj, {bool override: true, 
-                                          bool upsert: false, 
-                                          bool multiUpdate: false}) {
-    return mongoDb.update(collection, selector, obj, 
-        override: override, upsert: upsert, 
+  Future update(dynamic selector, T obj, {bool override: true,
+    bool upsert: false,
+    bool multiUpdate: false}) {
+    return mongoDb.update(collection, selector, obj,
+        override: override, upsert: upsert,
         multiUpdate: multiUpdate);
   }
-  
+
   /**
    * Wrapper for DbCollection.remove().
-   * 
+   *
    * [selector] can be a Map, a SelectorBuilder,
    * or an encodable object.
-   */ 
+   */
   Future remove(dynamic selector) {
     return mongoDb.remove(collection, selector);
   }
